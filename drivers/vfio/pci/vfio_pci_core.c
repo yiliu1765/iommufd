418686f8568e6b73e5df4d0c8d9440449ce7333d
Yi Liu <yi.l.liu@intel.com>
2021-11-22 16:50:10 +0800

    vfio_pci: Use vfio_alloc_device()
    Uses vfio_alloc_device() for the vfio_pci_core_device allocation.
    Free is done in vfio_pci_core_dealloc_device().
    
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/vfio/pci/vfio_pci_core.c
+++ drivers/vfio/pci/vfio_pci_core.c
@@ -1813,10 +1813,8 @@ static void vfio_pci_vga_uninit(struct vfio_pci_core_device *vdev)
 }
 
 void vfio_pci_core_init_device(struct vfio_pci_core_device *vdev,
-			       struct pci_dev *pdev,
-			       const struct vfio_device_ops *vfio_pci_ops)
+			       struct pci_dev *pdev)
 {
-	vfio_init_group_dev(&vdev->vdev, &pdev->dev, vfio_pci_ops);
 	vdev->pdev = pdev;
 	vdev->irq_type = VFIO_PCI_NUM_IRQS;
 	mutex_init(&vdev->igate);
@@ -1831,16 +1829,24 @@ void vfio_pci_core_init_device(struct vfio_pci_core_device *vdev,
 }
 EXPORT_SYMBOL_GPL(vfio_pci_core_init_device);
 
-void vfio_pci_core_uninit_device(struct vfio_pci_core_device *vdev)
+void vfio_pci_core_release(struct vfio_device *core_dev)
+{
+	struct vfio_pci_core_device *vdev =
+		container_of(core_dev, struct vfio_pci_core_device, vdev);
+
+	kfree(vdev->region);
+	kfree(vdev->pm_save);
+}
+EXPORT_SYMBOL_GPL(vfio_pci_core_release);
+
+void vfio_pci_core_dealloc_device(struct vfio_pci_core_device *vdev)
 {
 	mutex_destroy(&vdev->igate);
 	mutex_destroy(&vdev->ioeventfds_lock);
 	mutex_destroy(&vdev->vma_lock);
-	vfio_uninit_group_dev(&vdev->vdev);
-	kfree(vdev->region);
-	kfree(vdev->pm_save);
+	vfio_put_device(&vdev->vdev);
 }
-EXPORT_SYMBOL_GPL(vfio_pci_core_uninit_device);
+EXPORT_SYMBOL_GPL(vfio_pci_core_dealloc_device);
 
 int vfio_pci_core_register_device(struct vfio_pci_core_device *vdev)
 {
