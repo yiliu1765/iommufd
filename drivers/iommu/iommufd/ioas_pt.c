9194d1d84235002ef9e0ae8666bc205840e3be74
Liu Yi L <yi.l.liu@intel.com>
2021-12-26 23:58:10 -0800

    Return the expected buffer size to userspace for iova_range reporting
    Signed-off-by: Liu Yi L <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/ioas_pt.c
+++ drivers/iommu/iommufd/ioas_pt.c
@@ -94,6 +94,11 @@ int iommufd_ioas_pagetable_iova_ranges(struct iommufd_ucmd *ucmd)
 		}
 		cmd->out_num_iovas++;
 	}
+
+	if (cmd->out_num_iovas > max_iovas) {
+		cmd->size = sizeof(*cmd) + cmd->out_num_iovas * sizeof(cmd->out_valid_iovas[0]);
+	}
+
 	rc = iommufd_ucmd_respond(ucmd, sizeof(*cmd));
 out_put:
 	up_read(&ioaspt->iopt.rwsem);
