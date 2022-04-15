d493c504b049c357c0598c066ab6645bf43a861a
Christoph Hellwig <hch@lst.de>
2022-04-15 17:23:53 +0200

    vfio/mdev: unexport mdev_bus_type
    mdev_bus_type is only used in mdev.ko now, so unexport it.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- include/linux/mdev.h
+++ include/linux/mdev.h
@@ -92,8 +92,6 @@ static inline const guid_t *mdev_uuid(struct mdev_device *mdev)
 	return &mdev->uuid;
 }
 
-extern struct bus_type mdev_bus_type;
-
 int mdev_register_parent(struct mdev_parent *parent, struct device *dev,
 		struct mdev_driver *mdev_driver, struct mdev_type **types,
 		unsigned int nr_types);
