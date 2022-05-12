cfb63860d43d83bb30753cb26287c3f1036abf4d
Yi Liu <yi.l.liu@intel.com>
2022-05-12 04:20:00 -0700

    iommufd: Add cache invalidation support for hwpt
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/hw_pagetable.c
+++ drivers/iommu/iommufd/hw_pagetable.c
@@ -405,3 +405,27 @@ out_put_stage2:
 	iommufd_put_object(stage2_obj);
 	return rc;
 }
+
+int iommufd_hwpt_invalidate_cache(struct iommufd_ucmd *ucmd)
+{
+	struct iommu_hwpt_invalidate_s1_cache *cmd = ucmd->cmd;
+	struct iommufd_object *obj;
+	struct iommufd_hw_pagetable *hwpt;
+	int rc = 0;
+
+	if (cmd->flags)
+		return -EOPNOTSUPP;
+
+	/* TODO: more sanity check when the struct is finalized */
+	obj = iommufd_get_object(ucmd->ictx, cmd->hwpt_id,
+				 IOMMUFD_OBJ_HW_PAGETABLE_S1);
+	if (IS_ERR(obj))
+		return PTR_ERR(obj);
+
+	hwpt = container_of(obj, struct iommufd_hw_pagetable, obj);
+
+	iommu_domain_cache_inv(hwpt->domain, &cmd->info);
+
+	iommufd_put_object(obj);
+	return rc;
+}
