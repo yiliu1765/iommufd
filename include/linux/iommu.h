c2b38955b90e8185815394c4afa690036ca737e7
Lu Baolu <baolu.lu@linux.intel.com>
2022-06-08 10:03:15 +0800

    iommu: Add interface for iommu hardware information
    The interface looks like:
    
    int iommu_get_hw_info(struct device *dev,
    		      struct iommu_hw_info *info);
    
    The struct iommu_hw_info is defined like this:
    
    struct iommu_hw_info {
    	enum iommu_hw_type iommu_hw_type;
    	union {
    		/* IOMMU_DRIVER_INTEL_V1 */
    		struct iommu_vtd_data;
    	};
    };
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -42,6 +42,7 @@ struct iommu_domain_ops;
 struct notifier_block;
 struct iommu_fault_event;
 struct iommu_dma_cookie;
+struct iommu_hw_info;
 
 /* iommu fault flags */
 #define IOMMU_FAULT_READ	0x0
@@ -247,6 +248,7 @@ struct iommu_iotlb_gather {
  */
 struct iommu_ops {
 	bool (*capable)(enum iommu_cap);
+	int (*hw_info)(struct device *dev, struct iommu_hw_info *info);
 
 	/* Domain allocation and freeing by the iommu driver */
 	struct iommu_domain *(*domain_alloc)(unsigned iommu_domain_type);
@@ -411,6 +413,16 @@ struct dev_iommu {
 	void				*priv;
 	u32				max_pasids;
 };
+
+struct iommu_hw_info {
+	enum iommu_hw_type type;
+	unsigned int data_length;
+	union {
+		struct iommu_vtd_data vtd;
+	} data;
+};
+
+int iommu_get_hw_info(struct device *dev, struct iommu_hw_info *info);
 
 int iommu_device_register(struct iommu_device *iommu,
 			  const struct iommu_ops *ops,
@@ -1108,6 +1120,12 @@ static inline void
 iommu_domain_cache_inv(struct iommu_domain *domain,
 		       struct iommu_cache_invalidate_info *inv_info)
 {
+}
+
+static inline int
+iommu_get_hw_info(struct device *dev, struct iommu_hw_info *info)
+{
+	return -EINVAL;
 }
 #endif /* CONFIG_IOMMU_API */
 
