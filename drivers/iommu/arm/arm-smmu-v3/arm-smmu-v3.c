0da67bc6586cb6c7b7c345f8619734e8f4d966c1
Nicolin Chen <nicolinc@nvidia.com>
2022-07-07 00:15:58 -0700

    iommu/arm-smmu-v3: Implement arm_smmu_get_unmanaged_domain
    In a 1-stage translation setup, a device is attached to an iommu_domain
    (ARM_SMMU_DOMAIN_S1) that is IOMMU_DOMAIN_UNMANAGED type.
    
    In a 2-stage translation setup, a device is attached to an iommu_domain
    (ARM_SMMU_DOMAIN_S1) that is IOMMU_DOMAIN_NESTED type, which must have
    a valid "s2" pointer for an iommu_domain (ARM_SMMU_DOMAIN_S2) that is
    IOMMU_DOMAIN_UNMANAGED type.
    
    Add a function to return the correct iommu_domain pointer accordingly.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -2082,6 +2082,17 @@ static struct iommu_domain *arm_smmu_domain_alloc(unsigned type)
 	return &smmu_domain->domain;
 }
 
+static struct iommu_domain *arm_smmu_get_unmanaged_domain(struct device *dev)
+{
+	struct arm_smmu_master *master = dev_iommu_priv_get(dev);
+	struct arm_smmu_domain *smmu_domain = master->domain;
+
+	if (smmu_domain->s2)
+		return &smmu_domain->s2->domain;
+
+	return &smmu_domain->domain;
+}
+
 static int arm_smmu_bitmap_alloc(unsigned long *map, int span)
 {
 	int idx, size = 1 << span;
@@ -2878,6 +2889,7 @@ static struct iommu_ops arm_smmu_ops = {
 	.capable		= arm_smmu_capable,
 	.hw_info		= arm_smmu_hw_info,
 	.domain_alloc		= arm_smmu_domain_alloc,
+	.get_unmanaged_domain	= arm_smmu_get_unmanaged_domain,
 	.probe_device		= arm_smmu_probe_device,
 	.release_device		= arm_smmu_release_device,
 	.device_group		= arm_smmu_device_group,
