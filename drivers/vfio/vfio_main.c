20e18dcc93e60dcb48ad58e1a6133c7b6016d5cb
Jason Gunthorpe <jgg@nvidia.com>
2022-08-19 11:31:36 -0300

    vfio: Rename __vfio_group_unset_container()
    To vfio_container_detatch_group(). This function is really a container
    function.
    
    Fold the WARN_ON() into it as a precondition assertion.
    
    A following patch will move the vfio_container functions to their own .c
    file.
    
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/vfio/vfio_main.c
+++ drivers/vfio/vfio_main.c
@@ -926,12 +926,13 @@ static const struct file_operations vfio_fops = {
 /*
  * VFIO Group fd, /dev/vfio/$GROUP
  */
-static void __vfio_group_unset_container(struct vfio_group *group)
+static void vfio_container_detach_group(struct vfio_group *group)
 {
 	struct vfio_container *container = group->container;
 	struct vfio_iommu_driver *driver;
 
 	lockdep_assert_held_write(&group->group_rwsem);
+	WARN_ON(group->container_users != 1);
 
 	down_write(&container->group_lock);
 
@@ -979,7 +980,7 @@ static int vfio_group_ioctl_unset_container(struct vfio_group *group)
 		ret = -EBUSY;
 		goto out_unlock;
 	}
-	__vfio_group_unset_container(group);
+	vfio_container_detach_group(group);
 
 out_unlock:
 	up_write(&group->group_rwsem);
@@ -1331,10 +1332,8 @@ static int vfio_group_fops_release(struct inode *inode, struct file *filep)
 	 * is only called when there are no open devices.
 	 */
 	WARN_ON(group->notifier.head);
-	if (group->container) {
-		WARN_ON(group->container_users != 1);
-		__vfio_group_unset_container(group);
-	}
+	if (group->container)
+		vfio_container_detach_group(group);
 	group->opened_file = NULL;
 	up_write(&group->group_rwsem);
 
