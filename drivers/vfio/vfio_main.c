e67ac91ba036ccc00dead68910ae240b6d930227
Yi Liu <yi.l.liu@intel.com>
2022-12-15 23:49:56 -0800

    vfio: Block device access via device fd until device is opened
    Allow the vfio_device file to be in a state where the device FD is
    opened but the device cannot be used by userspace (i.e. its .open_device()
    hasn't been called). This inbetween state is not used when the device
    FD is spawned from the group FD, however when we create the device FD
    directly by opening a cdev it will be opened in the blocked state.
    
    The reason for the inbetween state is that userspace only gets a FD but
    doesn't gain access permission until binding the FD to an iommufd. So in
    the blocked state, only the bind operation is allowed. Completing bind
    will allow user to further access the device.
    
    This is implemented by adding a flag in struct vfio_device_file to mark
    the blocked state and using a simple smp_load_acquire() to obtain the
    flag value and serialize all the device setup with the thread accessing
    this device.
    
    Following this lockless scheme, it can safely handle the device FD
    unbound->bound but it cannot handle bound->unbound. To allow this we'd
    need to add a lock on all the vfio ioctls which seems costly. So once
    device FD is bound, it remains bound until the FD is closed.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- drivers/vfio/vfio_main.c
+++ drivers/vfio/vfio_main.c
@@ -1106,6 +1106,10 @@ static long vfio_device_fops_unl_ioctl(struct file *filep,
 	struct vfio_device *device = df->device;
 	int ret;
 
+	/* Paired with smp_store_release() in vfio_device_group_open() */
+	if (!smp_load_acquire(&df->access_granted))
+		return -EINVAL;
+
 	ret = vfio_device_pm_runtime_get(device);
 	if (ret)
 		return ret;
@@ -1133,6 +1137,10 @@ static ssize_t vfio_device_fops_read(struct file *filep, char __user *buf,
 	struct vfio_device_file *df = filep->private_data;
 	struct vfio_device *device = df->device;
 
+	/* Paired with smp_store_release() in vfio_device_group_open() */
+	if (!smp_load_acquire(&df->access_granted))
+		return -EINVAL;
+
 	if (unlikely(!device->ops->read))
 		return -EINVAL;
 
@@ -1146,6 +1154,10 @@ static ssize_t vfio_device_fops_write(struct file *filep,
 	struct vfio_device_file *df = filep->private_data;
 	struct vfio_device *device = df->device;
 
+	/* Paired with smp_store_release() in vfio_device_group_open() */
+	if (!smp_load_acquire(&df->access_granted))
+		return -EINVAL;
+
 	if (unlikely(!device->ops->write))
 		return -EINVAL;
 
@@ -1156,6 +1168,10 @@ static int vfio_device_fops_mmap(struct file *filep, struct vm_area_struct *vma)
 {
 	struct vfio_device_file *df = filep->private_data;
 	struct vfio_device *device = df->device;
+
+	/* Paired with smp_store_release() in vfio_device_group_open() */
+	if (!smp_load_acquire(&df->access_granted))
+		return -EINVAL;
 
 	if (unlikely(!device->ops->mmap))
 		return -EINVAL;
