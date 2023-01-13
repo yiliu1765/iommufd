9c7a09d1f66c86b175e8b811f04895650018e237
Yi Liu <yi.l.liu@intel.com>
2023-01-13 05:09:20 -0800

    iommufd: Add IOMMU_HWPT_INVALIDATE
    In nested translation, the stage-1 page table is user-managed and used by
    IOMMU hardware, so update of any present page table entry in the stage-1
    page table should be followed with an IOTLB invalidation.
    
    This adds IOMMU_HWPT_INVALIDATE for stage-1 IOTLB invalidation.
    
    Co-developed-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/hw_pagetable.c
+++ drivers/iommu/iommufd/hw_pagetable.c
@@ -373,3 +373,45 @@ out_put_idev:
 	iommufd_put_object(&idev->obj);
 	return rc;
 }
+
+int iommufd_hwpt_invalidate(struct iommufd_ucmd *ucmd)
+{
+	struct iommu_hwpt_invalidate *cmd = ucmd->cmd;
+	union iommu_cache_invalidate_user_data *data;
+	struct iommufd_hw_pagetable *hwpt;
+	u32 user_data_len, klen;
+	u64 user_ptr;
+	int rc = 0;
+
+	if (!cmd->data_len || cmd->__reserved)
+		return -EOPNOTSUPP;
+
+	hwpt = iommufd_get_hwpt(ucmd, cmd->hwpt_id);
+	if (IS_ERR(hwpt))
+		return PTR_ERR(hwpt);
+
+	klen = hwpt->domain->ops->cache_invalidate_user_data_len;
+	if (!hwpt->domain->ops->cache_invalidate_user || !klen) {
+		rc = -EINVAL;
+		goto out_put_hwpt;
+	}
+
+	/*
+	 * Copy the needed fields before reusing the ucmd buffer, this avoids
+	 * memory allocation in this path. Again, user invalidate data struct
+	 * must be added to union iommu_cache_invalidate_user_data.
+	 */
+	user_ptr = cmd->data_uptr;
+	user_data_len = cmd->data_len;
+	data = (union iommu_cache_invalidate_user_data *)cmd;
+
+	rc = copy_struct_from_user(data, klen,
+				   u64_to_user_ptr(user_ptr), user_data_len);
+	if (rc)
+		goto out_put_hwpt;
+
+	rc = hwpt->domain->ops->cache_invalidate_user(hwpt->domain, data);
+out_put_hwpt:
+	iommufd_put_object(&hwpt->obj);
+	return rc;
+}
