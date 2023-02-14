9b9eb7253190c691aa39736eb9ddc029c20e06e1
Yi Liu <yi.l.liu@intel.com>
2023-02-14 05:55:39 -0800

    vfio: Add VFIO_DEVICE_[AT|DE]TACH_IOMMUFD_PT
    This adds ioctl for userspace to attach device cdev fd to and detach
    from IOAS/hw_pagetable managed by iommufd.
    
        VFIO_DEVICE_ATTACH_IOMMUFD_PT: attach vfio device to IOAS, hw_pagetable
    				   managed by iommufd. Attach can be
    				   undo by VFIO_DEVICE_DETACH_IOMMUFD_PT
    				   or device fd close.
        VFIO_DEVICE_DETACH_IOMMUFD_PT: detach vfio device from the current attached
    				   IOAS or hw_pagetable managed by iommufd.
    
    Tested-by: Yanting Jiang <yanting.jiang@intel.com>
    Tested-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/vfio/iommufd.c
+++ drivers/vfio/iommufd.c
@@ -112,6 +112,24 @@ void vfio_iommufd_unbind(struct vfio_device_file *df)
 		vdev->ops->unbind_iommufd(vdev);
 }
 
+int vfio_iommufd_attach(struct vfio_device *vdev, u32 *pt_id)
+{
+	lockdep_assert_held(&vdev->dev_set->lock);
+
+	if (vfio_device_is_noiommu(vdev))
+		return 0;
+
+	return vdev->ops->attach_ioas(vdev, pt_id);
+}
+
+void vfio_iommufd_detach(struct vfio_device *vdev)
+{
+	lockdep_assert_held(&vdev->dev_set->lock);
+
+	if (!vfio_device_is_noiommu(vdev))
+		vdev->ops->detach_ioas(vdev);
+}
+
 struct iommufd_ctx *vfio_iommufd_physical_ictx(struct vfio_device *vdev)
 {
 	if (vdev->iommufd_device)
