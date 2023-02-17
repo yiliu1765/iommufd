2dc54dadf041dc50729e0a72f12b5f51c8960c66
Jason Gunthorpe <jgg@nvidia.com>
2023-02-17 15:30:04 -0400

    JUNK comment out annoying logging

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -1037,7 +1037,7 @@ rename:
 
 	trace_add_device_to_group(group->id, dev);
 
-	dev_info(dev, "Adding to iommu group %d\n", group->id);
+	// dev_info(dev, "Adding to iommu group %d\n", group->id);
 
 	return 0;
 
@@ -1074,7 +1074,7 @@ void iommu_group_remove_device(struct device *dev)
 	if (!group)
 		return;
 
-	dev_info(dev, "Removing from iommu group %d\n", group->id);
+	//dev_info(dev, "Removing from iommu group %d\n", group->id);
 
 	mutex_lock(&group->mutex);
 	list_for_each_entry(tmp_device, &group->devices, list) {
