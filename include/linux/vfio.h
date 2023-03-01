604b2cec8e0ce103052794b4afad45b1a7cd7402
Yi Liu <yi.l.liu@intel.com>
2023-03-01 02:40:48 -0800

    vfio: Add VFIO_DEVICE_BIND_IOMMUFD
    This adds ioctl for userspace to bind device cdev fd to iommufd.
    
        VFIO_DEVICE_BIND_IOMMUFD: bind device to an iommufd, hence gain DMA
    			      control provided by the iommufd. open_device
    			      op is called after bind_iommufd op.
    
    Tested-by: Yanting Jiang <yanting.jiang@intel.com>
    Tested-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Tested-by: Terrence Xu <terrence.xu@intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- include/linux/vfio.h
+++ include/linux/vfio.h
@@ -66,6 +66,7 @@ struct vfio_device {
 	struct iommufd_device *iommufd_device;
 	bool iommufd_attached;
 #endif
+	bool cdev_opened:1;
 };
 
 /**
@@ -170,7 +171,7 @@ vfio_iommufd_device_hot_reset_devid(struct vfio_device *vdev,
 
 static inline bool vfio_device_cdev_opened(struct vfio_device *device)
 {
-	return false;
+	return device->cdev_opened;
 }
 
 /**
