8b4722d5aac00b7b948bb7b5a29a468ffc40d18e
Yi Liu <yi.l.liu@intel.com>
2023-03-03 04:56:24 -0800

    iommufd: Pass in hwpt_type/user_data to iommufd_hw_pagetable_alloc()
    iommu core already supports accepting user_data and hwpt_type to allocate
    a user iommu_domain, which allows the caller iommufd_hw_pagetable_alloc()
    to accept the hwpt_type and user_data too. Thus, pass them in.
    
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Co-developed-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -259,7 +259,10 @@ struct iommufd_hw_pagetable {
 
 struct iommufd_hw_pagetable *
 iommufd_hw_pagetable_alloc(struct iommufd_ctx *ictx, struct iommufd_ioas *ioas,
-			   struct iommufd_device *idev, bool immediate_attach);
+			   struct iommufd_device *idev,
+			   enum iommu_hwpt_type hwpt_type,
+			   struct iommu_user_data *user_data,
+			   bool immediate_attach);
 int iommufd_hw_pagetable_enforce_cc(struct iommufd_hw_pagetable *hwpt);
 int iommufd_hw_pagetable_attach(struct iommufd_hw_pagetable *hwpt,
 				struct iommufd_device *idev);
