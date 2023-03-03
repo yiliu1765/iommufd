1be72b10830c29e9346556c75cb3b177eefb450c
Yi Liu <yi.l.liu@intel.com>
2023-03-03 05:06:13 -0800

    iommufd: Support IOMMU_HWPT_ALLOC allocation with user data
    IOMMU_HWPT_ALLOC already supports iommu_domain allocation for usersapce.
    But it can only allocate a hw_pagetable that associates to a given IOAS,
    i.e. only a kernel-managed hw_pagetable of IOMMU_HWPT_TYPE_DEFAULT type.
    
    IOMMU drivers can support other types of kernel-managed hw_pagetables,
    and even user-managed hw_pagetables in two-stage translation use cases.
    These hw_pagetables require user data inputs from the user spaces.
    
    Extends IOMMU_HWPT_ALLOC to accept user specified parameters and hwpt ID
    in @pt_id field.
    
    Co-developed-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/hw_pagetable.c
+++ drivers/iommu/iommufd/hw_pagetable.c
@@ -286,6 +286,7 @@ int iommufd_hwpt_alloc(struct iommufd_ucmd *ucmd)
 			struct iommu_user_data *user_data, bool flag);
 	struct iommufd_hw_pagetable *hwpt, *parent;
 	struct iommu_hwpt_alloc *cmd = ucmd->cmd;
+	struct iommu_user_data *data = NULL;
 	struct iommufd_object *pt_obj;
 	struct iommufd_device *idev;
 	struct iommufd_ioas *ioas;
@@ -294,6 +295,8 @@ int iommufd_hwpt_alloc(struct iommufd_ucmd *ucmd)
 
 	if (cmd->flags || cmd->__reserved)
 		return -EOPNOTSUPP;
+	if (!cmd->data_len && cmd->hwpt_type != IOMMU_HWPT_TYPE_DEFAULT)
+		return -EINVAL;
 
 	idev = iommufd_get_device(ucmd, cmd->dev_id);
 	if (IS_ERR(idev))
@@ -326,9 +329,18 @@ int iommufd_hwpt_alloc(struct iommufd_ucmd *ucmd)
 		goto out_put_pt;
 	}
 
+	if (cmd->data_len) {
+		data = kzalloc(sizeof(*data), GFP_KERNEL);
+		if (!data) {
+			rc = -ENOMEM;
+			goto out_put_pt;
+		}
+		data->uptr = u64_to_user_ptr(cmd->data_uptr);
+		data->len = cmd->data_len;
+	}
+
 	mutex_lock(mutex);
-	hwpt = alloc_fn(ucmd->ictx, pt_obj, idev,
-			IOMMU_HWPT_TYPE_DEFAULT, NULL, false);
+	hwpt = alloc_fn(ucmd->ictx, pt_obj, idev, cmd->hwpt_type, data, false);
 	if (IS_ERR(hwpt)) {
 		rc = PTR_ERR(hwpt);
 		goto out_unlock;
@@ -345,6 +357,7 @@ out_hwpt:
 	iommufd_object_abort_and_destroy(ucmd->ictx, &hwpt->obj);
 out_unlock:
 	mutex_unlock(mutex);
+	kfree(data);
 out_put_pt:
 	iommufd_put_object(pt_obj);
 out_put_idev:
