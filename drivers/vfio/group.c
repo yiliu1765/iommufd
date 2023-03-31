d8e16c9c6e38f745f42f97174e82749478cef10a
Yi Liu <yi.l.liu@intel.com>
2023-03-30 23:18:22 -0700

    vfio: Add vfio_register_pasid_iommu_dev()
    This adds vfio_register_pasid_iommu_dev() for device driver to register
    virtual devices which are isolated per PASID in physical IOMMU. The major
    usage is for the SIOV devices which allows the device driver to tag the
    DMAs out of virtual devices within it with different PASIDs.
    
    For the PASID virtual devices, there is no iommu group in concept. Hence
    the VFIO core only creates device user interface for such devices. This
    introduces a VFIO_PASID_IOMMU group type to differentiate from the other
    devices that have iommu group in concept.
    
    Signed-off-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/vfio/group.c
+++ drivers/vfio/group.c
@@ -400,6 +400,9 @@ int vfio_device_block_group(struct vfio_device *device)
 	struct vfio_group *group = device->group;
 	int ret = 0;
 
+	if (!group)
+		return 0;
+
 	mutex_lock(&group->group_lock);
 	if (group->opened_file) {
 		ret = -EBUSY;
@@ -417,6 +420,8 @@ void vfio_device_unblock_group(struct vfio_device *device)
 {
 	struct vfio_group *group = device->group;
 
+	if (!group)
+		return;
 	mutex_lock(&group->group_lock);
 	group->cdev_device_open_cnt--;
 	mutex_unlock(&group->group_lock);
@@ -697,6 +702,10 @@ int vfio_device_set_group(struct vfio_device *device,
 {
 	struct vfio_group *group;
 
+	/* No group associate with a device with pasid */
+	if (type == VFIO_PASID_IOMMU)
+		return 0;
+
 	if (type == VFIO_IOMMU)
 		group = vfio_group_find_or_alloc(device->dev);
 	else
@@ -714,6 +723,9 @@ void vfio_device_remove_group(struct vfio_device *device)
 {
 	struct vfio_group *group = device->group;
 	struct iommu_group *iommu_group;
+
+	if (!group)
+		return;
 
 	if (group->type == VFIO_NO_IOMMU || group->type == VFIO_EMULATED_IOMMU)
 		iommu_group_remove_device(device->dev);
@@ -759,6 +771,9 @@ void vfio_device_remove_group(struct vfio_device *device)
 
 void vfio_device_group_register(struct vfio_device *device)
 {
+	if (!device->group)
+		return;
+
 	mutex_lock(&device->group->device_lock);
 	list_add(&device->group_next, &device->group->device_list);
 	mutex_unlock(&device->group->device_lock);
@@ -766,6 +781,9 @@ void vfio_device_group_register(struct vfio_device *device)
 
 void vfio_device_group_unregister(struct vfio_device *device)
 {
+	if (!device->group)
+		return;
+
 	mutex_lock(&device->group->device_lock);
 	list_del(&device->group_next);
 	mutex_unlock(&device->group->device_lock);
