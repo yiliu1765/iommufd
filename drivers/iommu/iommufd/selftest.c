610347268f9ba11b3102d3e7e9eb1ce08f4aa21b
Nicolin Chen <nicolinc@nvidia.com>
2023-04-22 17:43:10 -0700

    iommufd/selftest: Add IOMMU_TEST_OP_DEV_CHECK_DATA
    Add mock_domain_set/unset_dev_user_data and iommufd_test_dev_check_data to
    allow testing on IOMMUFD_CMD_SET/UNSET_DEV_DATA ioctls from the user space
    selftest via IOMMU_TEST_OP_DEV_CHECK_DATA.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- drivers/iommu/iommufd/selftest.c
+++ drivers/iommu/iommufd/selftest.c
@@ -98,6 +98,7 @@ enum selftest_obj_type {
 
 struct mock_dev {
 	struct device dev;
+	u32 dev_data;
 };
 
 struct selftest_obj {
@@ -359,6 +360,28 @@ static void mock_domain_set_plaform_dma_ops(struct device *dev)
 	 */
 }
 
+static int mock_domain_set_dev_user_data(struct device *dev,
+					 const struct iommu_user_data *user_data)
+{
+	const size_t min_len = offsetofend(struct iommu_test_device_data, val);
+	struct mock_dev *mdev = container_of(dev, struct mock_dev, dev);
+	struct iommu_test_device_data data;
+	int rc;
+
+	rc = iommu_copy_user_data(&data, user_data, sizeof(data), min_len);
+	if (rc)
+		return rc;
+	mdev->dev_data = data.val;
+	return 0;
+}
+
+static void mock_domain_unset_dev_user_data(struct device *dev)
+{
+	struct mock_dev *mdev = container_of(dev, struct mock_dev, dev);
+
+	mdev->dev_data = 0;
+}
+
 static struct iommu_device mock_iommu_device = {
 };
 
@@ -375,6 +398,8 @@ static const struct iommu_ops mock_ops = {
 	.domain_alloc_user = mock_domain_alloc_user,
 	.capable = mock_domain_capable,
 	.set_platform_dma_ops = mock_domain_set_plaform_dma_ops,
+	.set_dev_user_data = mock_domain_set_dev_user_data,
+	.unset_dev_user_data = mock_domain_unset_dev_user_data,
 	.device_group = generic_device_group,
 	.probe_device = mock_probe_device,
 	.default_domain_ops =
@@ -718,6 +743,24 @@ static int iommufd_test_md_check_iotlb(struct iommufd_ucmd *ucmd,
 	if (iotlb != mock->iotlb)
 		rc = -EINVAL;
 	iommufd_put_object(&hwpt->obj);
+	return rc;
+}
+
+static int iommufd_test_dev_check_data(struct iommufd_ucmd *ucmd,
+				       u32 dev_id, u32 val)
+{
+	struct iommufd_device *idev;
+	struct mock_dev *mdev;
+	int rc = 0;
+
+	idev = iommufd_get_device(ucmd, dev_id);
+	if (IS_ERR(idev))
+		return PTR_ERR(idev);
+	mdev = container_of(idev->dev, struct mock_dev, dev);
+
+	if (mdev->dev_data != val)
+		rc = -EINVAL;
+	iommufd_put_object(&idev->obj);
 	return rc;
 }
 
@@ -1137,6 +1180,9 @@ int iommufd_test(struct iommufd_ucmd *ucmd)
 	case IOMMU_TEST_OP_MD_CHECK_IOTLB:
 		return iommufd_test_md_check_iotlb(ucmd, cmd->id,
 						   cmd->check_iotlb.iotlb);
+	case IOMMU_TEST_OP_DEV_CHECK_DATA:
+		return iommufd_test_dev_check_data(ucmd, cmd->id,
+						   cmd->check_dev_data.val);
 	case IOMMU_TEST_OP_CREATE_ACCESS:
 		return iommufd_test_create_access(ucmd, cmd->id,
 						  cmd->create_access.flags);
