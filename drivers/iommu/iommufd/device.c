d28677f367e01911c4d35a9a1d575e1cc9b407b1
Kevin Tian <kevin.tian@intel.com>
2023-05-22 00:03:16 -0700

    iommufd: Support attach/replace hwpt per pasid
    This introduces three APIs for device drivers to manage pasid attach/
    replace/detach.
    
        int iommufd_device_pasid_attach(struct iommufd_device *idev,
    				    u32 pasid, u32 *pt_id);
        int iommufd_device_pasid_replace(struct iommufd_device *idev,
    				     u32 pasid, u32 *pt_id);
        void iommufd_device_pasid_detach(struct iommufd_device *idev,
    				     u32 pasid);
    
    pasid operations have different implications when comparing to device
    operations:
    
     - No connection to iommufd_group since pasid is a device capability
       and can be enabled only in singleton group;
    
     - no reserved region per pasid otherwise SVA architecture is already
       broken (CPU address space doesn't count device reserved regions);
    
     - accordingly no sw_msi trick;
    
     - immediated_attach is not supported, expecting that arm-smmu driver
       will already remove that requirement before supporting this pasid
       operation. This avoids unnecessary change in iommufd_hw_pagetable_alloc()
       to carry the pasid from device.c.
    
    With above differences, this puts all pasid related logics into a new
    pasid.c file.
    
    Cache coherency enforcement is still applied to pasid operations since
    it is about memory accesses post page table walking (no matter the walk
    is per RID or per PASID).
    
    Since the attach is per PASID, this introduces a pasid_hwpts xarray to
    track the per-pasid attach data.
    
    Signed-off-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/device.c
+++ drivers/iommu/iommufd/device.c
@@ -136,6 +136,7 @@ void iommufd_device_destroy(struct iommufd_object *obj)
 	struct iommufd_device *idev =
 		container_of(obj, struct iommufd_device, obj);
 
+	WARN_ON(!xa_empty(&idev->pasid_hwpts));
 	iommu_device_release_dma_owner(idev->dev);
 	iommufd_put_group(idev->igroup);
 	if (!iommufd_selftest_is_mock_dev(idev->dev))
@@ -215,6 +216,8 @@ struct iommufd_device *iommufd_device_bind(struct iommufd_ctx *ictx,
 	refcount_inc(&idev->obj.users);
 	/* igroup refcount moves into iommufd_device */
 	idev->igroup = igroup;
+
+	xa_init(&idev->pasid_hwpts);
 
 	/*
 	 * If the caller fails after this success it must call
@@ -534,7 +537,9 @@ err_unlock:
 static struct iommufd_hw_pagetable *do_attach(struct iommufd_device *idev,
 		struct iommufd_hw_pagetable *hwpt, struct attach_data *data)
 {
-	return data->attach_fn(idev, hwpt);
+	return data->pasid == IOMMU_PASID_INVALID ?
+		data->attach_fn(idev, hwpt) :
+		data->pasid_attach_fn(idev, data->pasid, hwpt);
 }
 
 /*
@@ -684,6 +689,7 @@ int iommufd_device_attach(struct iommufd_device *idev, u32 *pt_id)
 	int rc;
 	struct attach_data data = {
 		.attach_fn = &iommufd_device_do_attach,
+		.pasid = IOMMU_PASID_INVALID
 	};
 
 	rc = iommufd_device_change_pt(idev, pt_id, &data);
@@ -718,6 +724,7 @@ int iommufd_device_replace(struct iommufd_device *idev, u32 *pt_id)
 {
 	struct attach_data data = {
 		.attach_fn = &iommufd_device_do_replace,
+		.pasid = IOMMU_PASID_INVALID
 	};
 
 	return iommufd_device_change_pt(idev, pt_id, &data);
