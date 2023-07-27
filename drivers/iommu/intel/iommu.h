a4cb20a9c830260f9aa5286c36f84f9c9a15050f
Lu Baolu <baolu.lu@linux.intel.com>
2023-07-27 21:06:52 +0800

    iommu/vt-d: Make prq draining code generic
    Currently draining page requests and responses for a pasid is part of SVA
    implementation. This is because the driver only supports attaching an SVA
    domain to a device pasid. As we are about to support attaching other types
    of domains to a device pasid, the prq draining code becomes generic.
    
    Reviewed-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>

--- drivers/iommu/intel/iommu.h
+++ drivers/iommu/intel/iommu.h
@@ -887,6 +887,7 @@ int intel_svm_page_response(struct device *dev, struct iommu_fault_event *evt,
 			    struct iommu_page_response *msg);
 struct iommu_domain *intel_svm_domain_alloc(void);
 void intel_svm_remove_dev_pasid(struct device *dev, ioasid_t pasid);
+void intel_drain_pasid_prq(struct device *dev, u32 pasid);
 
 struct intel_svm_dev {
 	struct list_head list;
@@ -905,6 +906,7 @@ struct intel_svm {
 };
 #else
 static inline void intel_svm_check(struct intel_iommu *iommu) {}
+static inline void intel_drain_pasid_prq(struct device *dev, u32 pasid) {}
 static inline struct iommu_domain *intel_svm_domain_alloc(void)
 {
 	return NULL;
