ae4863ae696d3254be00b50ad9f914ff6af63d02
Lu Baolu <baolu.lu@linux.intel.com>
2023-07-27 21:05:54 +0800

    iommu/vt-d: Add domain_flush_pasid_iotlb()
    The VT-d spec requires to use PASID-based-IOTLB invalidation descriptor
    to invalidate IOTLB and the paging-structure caches for a first-stage
    page table. Add a generic helper to do this.
    
    RID2PASID is used if the domain has been attached to a physical device,
    otherwise real PASIDs that the domain has been attached to will be used.
    The 'real' PASID attachment is handled in the subsequent change.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1466,6 +1466,18 @@ static void iommu_flush_dev_iotlb(struct dmar_domain *domain,
 	spin_unlock_irqrestore(&domain->lock, flags);
 }
 
+static void domain_flush_pasid_iotlb(struct intel_iommu *iommu,
+				     struct dmar_domain *domain, u64 addr,
+				     unsigned long npages, bool ih)
+{
+	u16 did = domain_id_iommu(domain, iommu);
+	unsigned long flags;
+
+	spin_lock_irqsave(&domain->lock, flags);
+	qi_flush_piotlb(iommu, did, IOMMU_NO_PASID, addr, npages, ih);
+	spin_unlock_irqrestore(&domain->lock, flags);
+}
+
 void iommu_flush_iotlb_psi(struct intel_iommu *iommu,
 			   struct dmar_domain *domain,
 			   unsigned long pfn, unsigned int pages,
@@ -1483,7 +1495,7 @@ void iommu_flush_iotlb_psi(struct intel_iommu *iommu,
 		ih = 1 << 6;
 
 	if (domain->use_first_level) {
-		qi_flush_piotlb(iommu, did, IOMMU_NO_PASID, addr, pages, ih);
+		domain_flush_pasid_iotlb(iommu, domain, addr, pages, ih);
 	} else {
 		unsigned long bitmask = aligned_pages - 1;
 
@@ -1553,7 +1565,7 @@ void intel_flush_iotlb_all(struct iommu_domain *domain)
 		u16 did = domain_id_iommu(dmar_domain, iommu);
 
 		if (dmar_domain->use_first_level)
-			qi_flush_piotlb(iommu, did, IOMMU_NO_PASID, 0, -1, 0);
+			domain_flush_pasid_iotlb(iommu, dmar_domain, 0, -1, 0);
 		else
 			iommu->flush.flush_iotlb(iommu, did, 0, 0,
 						 DMA_TLB_DSI_FLUSH);
