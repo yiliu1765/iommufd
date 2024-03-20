93a95f2c17be0b5ccf55111d417816d64c019bb0
Yi Liu <yi.l.liu@intel.com>
2024-03-20 06:02:22 -0700

    iommu: Introduce a replace API for device pasid
    Provide a high-level API to allow replacements of one domain with
    another for specific pasid of a device. This is similar to
    iommu_group_replace_domain() and it is also expected to be used
    only by IOMMUFD.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -3304,13 +3304,15 @@ bool iommu_group_dma_owner_claimed(struct iommu_group *group)
 EXPORT_SYMBOL_GPL(iommu_group_dma_owner_claimed);
 
 static int __iommu_set_group_pasid(struct iommu_domain *domain,
-				   struct iommu_group *group, ioasid_t pasid)
+				   struct iommu_group *group, ioasid_t pasid,
+				   struct iommu_domain *old)
 {
 	struct group_device *device;
 	int ret = 0;
 
 	for_each_group_device(group, device) {
-		ret = domain->ops->set_dev_pasid(domain, device->dev, pasid, NULL);
+		ret = domain->ops->set_dev_pasid(domain, device->dev,
+						 pasid, old);
 		if (ret)
 			break;
 	}
@@ -3330,6 +3332,47 @@ static void __iommu_remove_group_pasid(struct iommu_group *group,
 	}
 }
 
+static int __iommu_group_attach_pasid(struct iommu_domain *domain,
+				      struct iommu_group *group,
+				      ioasid_t pasid, bool replace)
+{
+	void *curr;
+	int ret;
+
+	lockdep_assert_held(&group->mutex);
+
+	if (replace) {
+		curr = xa_store(&group->pasid_array, pasid,
+				domain, GFP_KERNEL);
+		ret = xa_err(curr);
+		if (ret)
+			return ret;
+		if (curr == domain)
+			return 0;
+	} else {
+		curr = xa_cmpxchg(&group->pasid_array, pasid, NULL, domain,
+				  GFP_KERNEL);
+		if (curr)
+			return xa_err(curr) ? : -EBUSY;
+	}
+
+	ret = __iommu_set_group_pasid(domain, group, pasid, curr);
+	if (ret) {
+		__iommu_remove_group_pasid(group, pasid);
+		xa_erase(&group->pasid_array, pasid);
+		/*
+		 * Rollback if there is already a domain, and it is
+		 * a driver bug to fail attach with a previously good
+		 * domain.
+		 */
+		if (curr)
+			WARN_ON(__iommu_set_group_pasid(curr, group,
+							pasid, curr));
+	}
+
+	return ret;
+}
+
 /*
  * iommu_attach_device_pasid() - Attach a domain to pasid of device
  * @domain: the iommu domain.
@@ -3343,7 +3386,6 @@ int iommu_attach_device_pasid(struct iommu_domain *domain,
 {
 	/* Caller must be a probed driver on dev */
 	struct iommu_group *group = dev->iommu_group;
-	void *curr;
 	int ret;
 
 	if (!domain->ops->set_dev_pasid)
@@ -3356,22 +3398,42 @@ int iommu_attach_device_pasid(struct iommu_domain *domain,
 		return -EINVAL;
 
 	mutex_lock(&group->mutex);
-	curr = xa_cmpxchg(&group->pasid_array, pasid, NULL, domain, GFP_KERNEL);
-	if (curr) {
-		ret = xa_err(curr) ? : -EBUSY;
-		goto out_unlock;
-	}
-
-	ret = __iommu_set_group_pasid(domain, group, pasid);
-	if (ret) {
-		__iommu_remove_group_pasid(group, pasid);
-		xa_erase(&group->pasid_array, pasid);
-	}
-out_unlock:
+	ret = __iommu_group_attach_pasid(domain, group, pasid, false);
 	mutex_unlock(&group->mutex);
 	return ret;
 }
 EXPORT_SYMBOL_GPL(iommu_attach_device_pasid);
+
+/**
+ * iommu_replace_device_pasid - replace the domain that a pasid is attached to
+ * @domain: new IOMMU domain to replace with
+ * @dev: the physical device
+ * @pasid: pasid that will be attached to the new domain
+ *
+ * This API allows the pasid to switch domains. Return 0 on success, or an
+ * error. The pasid will roll back to use the old domain if failure. The
+ * caller could call iommu_detach_device_pasid() before free the old domain
+ * in order to avoid use-after-free case.
+ */
+int iommu_replace_device_pasid(struct iommu_domain *domain,
+			       struct device *dev, ioasid_t pasid)
+{
+	/* Caller must be a probed driver on dev */
+	struct iommu_group *group = dev->iommu_group;
+	int ret;
+
+	if (!domain)
+		return -EINVAL;
+
+	if (!group)
+		return -ENODEV;
+
+	mutex_lock(&group->mutex);
+	ret = __iommu_group_attach_pasid(domain, group, pasid, true);
+	mutex_unlock(&group->mutex);
+	return ret;
+}
+EXPORT_SYMBOL_NS_GPL(iommu_replace_device_pasid, IOMMUFD_INTERNAL);
 
 /*
  * iommu_detach_device_pasid() - Detach the domain from pasid of device
