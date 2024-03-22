20ecf8868515c827622a48d944cdcf13ea17790e
Yi Liu <yi.l.liu@intel.com>
2024-03-22 01:10:33 -0700

    iommu: Introduce a replace API for device pasid
    Provide a high-level API to allow replacements of one domain with
    another for specific pasid of a device. This is similar to
    iommu_group_replace_domain_handle() and it is expected to be used
    only by IOMMUFD.
    
    Co-developed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommu-priv.h
+++ drivers/iommu/iommu-priv.h
@@ -43,4 +43,8 @@ void iommu_detach_group_handle(struct iommu_domain *domain,
 int iommu_replace_group_handle(struct iommu_group *group,
 			       struct iommu_domain *new_domain,
 			       struct iommu_attach_handle *handle);
+
+int iommu_replace_device_pasid_handle(struct iommu_domain *domain,
+				      struct device *dev, ioasid_t pasid,
+				      struct iommu_attach_handle *handle);
 #endif /* __LINUX_IOMMU_PRIV_H */
