7a5477c80e79517bea97917411a19d18cbeb602e
Yi Liu <yi.l.liu@intel.com>
2025-02-26 03:40:25 -0800

    iommufd: Enforce PASID-compatible domain in PASID path
    AMD IOMMU requires attaching PASID-compatible domains to PASID-capable
    devices. This includes the domains attached to RID and PASIDs. Related
    discussions in link [1] and [2]. ARM also has such a requirement, Intel
    does not need it, but can live up with it. Hence, iommufd is going to
    enforce this requirement as it is not harmful to vendors that do not
    need it.
    
    Mark the PASID-compatible domains and enforce it in the PASID path.
    
    [1] https://lore.kernel.org/linux-iommu/20240709182303.GK14050@ziepe.ca/
    [2] https://lore.kernel.org/linux-iommu/20240822124433.GD3468552@ziepe.ca/
    
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -296,6 +296,7 @@ struct iommufd_hw_pagetable {
 	struct iommufd_object obj;
 	struct iommu_domain *domain;
 	struct iommufd_fault *fault;
+	bool pasid_compat : 1;
 };
 
 struct iommufd_hwpt_paging {
